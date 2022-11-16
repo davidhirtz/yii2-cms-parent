6686539e861552a72cd87369ab42c1c0410add66
davidhirtz <david.hirtz@me.com>
2022-11-16 12:46:28 +0100

    Improved `whereSlug`method

--- models/queries/EntryQuery.php
+++ models/queries/EntryQuery.php
@@ -2,6 +2,8 @@
 
 namespace davidhirtz\yii2\cms\parent\models\queries;
 
+use davidhirtz\yii2\cms\models\Entry;
+
 /**
  * EntryQuery extends the entry query class by allowing slug look-ups via `parent_slug`. This file must be manually
  * added to the project.
@@ -25,14 +27,23 @@ class EntryQuery extends \davidhirtz\yii2\cms\models\queries\EntryQuery
     }
 
     /**
+     * Extends the default `whereSlug` method to allow for look-ups via `parent_slug` if the attribute is part of the
+     * unique URL target.
+     *
      * @param string $slug
      * @return EntryQuery
      */
     public function whereSlug(string $slug)
     {
-        $slug = explode('/', $slug);
+        if (in_array('parent_slug', Entry::instance()->slugTargetAttribute ?? [])) {
+            $slug = explode('/', $slug);
 
-        return parent::whereSlug(array_pop($slug))
-            ->andWhere(['parent_slug' => implode('/', $slug)]);
+            return $this->andWhere([
+                $this->getI18nAttributeName('slug') => array_pop($slug),
+                $this->getI18nAttributeName('parent_slug') => implode('/', $slug),
+            ]);
+        }
+
+        return parent::whereSlug($slug);
     }
 }
